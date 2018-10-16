e8f3f4b2235677a3d4217a29aa433612ded563bf
Maksim Ustichenko <nix.n0uk@gmail.com>
2018-10-16 17:39:51 +0700

    Initial commit

---
+++ Assets/Scripts/AutoEquip.cs
@@ -0,0 +1,27 @@
+﻿using Spine;
+using Spine.Unity;
+using Spine.Unity.Modules.AttachmentTools;
+using UnityEngine;
+
+namespace SpineTestLeak
+{
+    public class AutoEquip : MonoBehaviour
+    {
+        [SerializeField] private SkeletonAnimation _skeletonAnimation;
+        [SerializeField] private Sprite _attachment;
+
+        private void Start()
+        {
+            var slotIndex = _skeletonAnimation.skeleton.FindSlotIndex("sword");
+            var templateSkin = _skeletonAnimation.SkeletonDataAsset.GetSkeletonData(false).FindSkin("default");
+            var templateAttachment = templateSkin.GetAttachment(slotIndex, "sword");
+            var attachment = templateAttachment.GetRemappedClone(_attachment,
+                _skeletonAnimation.skeletonDataAsset.atlasAssets[0].materials[0], true);
+            var skin = new Skin("EquipSkin");
+            skin.Append(_skeletonAnimation.SkeletonDataAsset.GetSkeletonData(false).FindSkin("knight"));
+            skin.AddAttachment(slotIndex, "sword", attachment);
+            _skeletonAnimation.Skeleton.SetSkin(skin);
+            _skeletonAnimation.Skeleton.SetSlotsToSetupPose();
+        }
+    }
+}
